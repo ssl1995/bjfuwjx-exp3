2a16c71fc9457b9a2b5f94887c94491d7216514f
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-05 18:47:27 +0800

    init2

---
+++ 5200090宋声林/源代码/bjfuwjx/exp1/ConfigurableDrawing.java
@@ -0,0 +1,23 @@
+import javax.script.ScriptEngineManager;
+import javax.swing.*;
+
+/**
+ * @Author: SongShengLin
+ * @Date: 2022/05/05 3:27 PM
+ * @Describe: 绘图软件
+ */
+public class ConfigurableDrawing {
+
+
+    private JFrame frame = new JFrame("简单绘图");
+
+    private void init() {
+        frame.setSize(2000,2000);
+        
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 5200090宋声林/源代码/bjfuwjx/exp1/DrawPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * @Author: SongShengLin
+ * @Date: 2022/05/05 4:03 PM
+ * @Describe:
+ */
+public class DrawPanel extends JPanel {
+
+}
